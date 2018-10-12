3e4970a388a30507cd1ba388110ecbdb5e3a04da
Lamoliveira <LamOliveira@gmail.com>
2018-10-11 20:37:55 -0400

    trying some stuff

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
