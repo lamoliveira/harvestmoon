7f7ab98904ec3a81091d1e3f9513edc1bf616dd6
thomson3914 <thomson3914@gmail.com>
2018-10-19 13:33:14 -0400

    changed about grow share hero wrapper

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => (
-  <footer className="footer">
-    <span>Harvest Moon 2018 - Luiz and Tyler</span>
+  <footer className="footer text-center">
+    <span>© Harvest Moon 2018 - Luiz and Tyler</span>
   </footer>
 );
 
